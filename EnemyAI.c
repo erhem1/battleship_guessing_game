b74b53250644018dcaa95de63ff97a126f1c1069
syintigs <erhembaatardashdorj@gmail.com>
2020-07-15 23:17:53 +0800

    EnemyAI.c. Updated the functions ai_x() and ai_y() alternate between subtracting and adding random numbers to middle_position

--- EnemyAI.c
+++ EnemyAI.c
@@ -6,13 +6,27 @@ int ai_x(int middle_position, int difficulty)
 {
 	int x = 0; 
 	srand(time(NULL));
-	while(1)
+	if( (rand() % 2) == 0 )  // 50% chance to add the random number to middle_position
 	{
-		if(difficulty == EASY)          x = rand() % 4;
-		else if(difficulty == MEDIUM)   x = rand() % 3;
-		else if(difficulty == HARD)     x = rand() % 2;
-		else continue;
-		if( ( x>= 0 ) && ( x<100) ) return x;
+		while(1)
+		{
+			if(difficulty == EASY)          x = middle_position + (rand() % 4);
+			else if(difficulty == MEDIUM)   x = middle_position + (rand() % 3);
+			else if(difficulty == HARD)     x = middle_position + (rand() % 2);
+			else return -1; // Not a proper difficulty as argument
+			if( ( x>= 0 ) && ( x<100) ) return x;
+		}
+	}
+	else  			// 50% chance to minus the random number from  middle_position 
+	{
+		while(1)
+		{
+			if(difficulty == EASY)          x = middle_position - (rand() % 4);
+			else if(difficulty == MEDIUM)   x = middle_position - (rand() % 3);
+			else if(difficulty == HARD)     x = middle_position - (rand() % 2);
+			else return -1; // Not a proper difficulty as argument
+			if( ( x>= 0 ) && ( x<100) ) return x;
+		}
 	}
 
 }
@@ -21,13 +35,27 @@ int ai_y(int middle_position, int difficulty)
 {
 	int y = 0;
 	srand(time(NULL));
-	while(1)
+	if( (rand() % 2) == 0 ) // 50% chance to add the random number to middle_position
 	{
-		if(difficulty == EASY)         y = rand() % 4;
-		else if(difficulty == MEDIUM)  y = rand() % 3;
-		else if(difficulty == HARD)    y = rand() %2;
-		else continue;
-		if( ( y>=0 ) && ( y<100 ) ) return y;
+		while(1)
+		{
+			if(difficulty == EASY)         y = middle_position + (rand() % 4);
+			else if(difficulty == MEDIUM)  y = middle_position + (rand() % 3);
+			else if(difficulty == HARD)    y = middle_position + (rand() % 2);
+			else return -1; // Not a proper difficulty as argument
+			if( ( y>=0 ) && ( y<100 ) ) return y;
+		}
+	}
+	else			// 50% chance to subtract te random from middle_position   
+	{
+		while(1)
+		{
+			if(difficulty == EASY)         y = middle_position - (rand() %4);
+			else if(difficulty == MEDIUM)  y = middle_position - (rand() %3);
+			else if(difficulty == HARD)    y = middle_position - (rand() %2);
+			else return -1; // Not a proper difficulty as argument
+			if( ( y>=0 ) && ( y<100 ) ) return y;
+		}
 	}
 }
 
