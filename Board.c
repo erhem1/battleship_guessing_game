c78c39e678dd34bf787ed76c759cd615afd17193
Juan Carlos <zolmonjuancarlos@gmail.com>
2020-07-16 12:44:21 +0800

    custom board symbols made

--- Board.c
+++ Board.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include "GlobalVariables.h"
 
+#define GREEN "\x1b[32m"
+#define RED "\x1b[31m"
+#define BLUE  "\x1b[34m"
+#define COLORRESET "\x1b[0m"
+#define SEA "#"
+#define SHIP "@"
+#define ENEMYSHIP "%"
+
 void printBoard()
 {
 	for(int i =0; i<=10;i++)
 	{
 		for(int j=0; j<=10;j++)
 		{
-			printf("%d ",board[i][j]);
+			switch(board[i][j])
+			{
+				case EMPTY:
+					printf("%s,%s,%s"BLUE,SEA,COLORRESET);
+					
+			}
 			if(j==10)
 			{
 				printf("\n");
