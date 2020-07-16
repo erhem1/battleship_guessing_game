359427c891fc79ac17a0390f17d58755fb2e9c84
Juan Carlos <zolmonjuancarlos@gmail.com>
2020-07-16 15:41:42 +0800

    enemy ai might build a ship if this works

--- EnemyBuild.c
+++ EnemyBuild.c
@@ -2,11 +2,17 @@ void buildShips()
 {
 	for(int i = 0; i<5;i++)
 	{
+		bool cantBuild=false;
 		int xCoordinate = rand() % 9;
 		int yCoordinate = rand() % 9; //chooses a random location on the coordinate plane
 		bool shipOrientation= rand() %1;
-		if(shipOrientation)//chooses the orientation of the ship. in which one is horizontal
+
+		if(cantBuild&& i==1)
 		{
+			i=0;
+			cantBuild=false;
+		}
+
 			do{
 				int startSize=0;
 				int maxSize=4;
@@ -21,15 +27,51 @@ void buildShips()
 						break;
 					case 2://space is already taken by own ship so continue to build onto ship
 						bool direction = rand() %1;
-						if(direction)//checks wether to build left or right
+						if(direction)//checks wether to build left/down or right/up
 						{
-							if(canBuild(xCoordinate+1,yCooridnate)==2)
+							if(shipOrientation)
 							{
-								board[yCoordinate,xCoordinate]==ENEMYSHIP;
+								if(canBuild(xCoordinate+1,yCooridnate)==2)
+								{
+								board[yCoordinate,xCoordinate+1]=ENEMYSHIP;
+								}	
+							}
+							else
+							{
+								if(canBuild(xCoordinate,yCooridnate+1)==2)
+								{
+								board[yCoordinate+1,xCoordinate]=ENEMYSHIP;
+								}
+							}
+						}
+						else
+						{
+							if(shipOrientation)
+							{
+								if(canBuild(xCoordinate-1,yCooridnate)==2)
+								{
+									board[yCoordinate,xCoordinate-1]=ENEMYSHIP;
+								}
+							}
+							else
+							{
+								if(canBuild(xCoordinate,yCooridnate-1)==2)
+								{
+								board[yCoordinate-1,xCoordinate]=ENEMYSHIP;
+								}
 							}
 						}
 						break;
 					case 0:
+						if(i==0)
+						{
+							cantBuild=true;
+						}
+						else
+						{
+							--i;
+						}
+						break;
 
 				}
 
@@ -38,15 +80,14 @@ void buildShips()
 				{
 					break;
 				}
-			}while(rand()%1);//chooses wether to continue building or build new ship
+				int expand = rand() %1;
+				if(expand)
+				{
+					++i;
+				}
+			}while(expand);//chooses wether to continue building or build new ship
 			xCoordinate = rand() %9;
 			yCoordinate = rand() %9;
-
-		}
-		else
-		{
-
-		}
 		
 	}
 
@@ -56,15 +97,15 @@ int canBuild(int xCoor,int yCoor)
 {
 	if(!xCoor<0 && !xCoor>10 && !yCoor<0 && !yCoor>10)
 	{
-		if(board[yCoor,xCoor]!=FRIENDLY&& board[yCoor,xCoor]!=ENEMY)
+		if(board[yCoor,xCoor]!=FRIENDLY&& board[yCoor,xCoor]!=ENEMY) //sea is empty
 		{
 			return 1;
 		}
-		else if(board[yCoord,xCoor]==ENEMY)
+		else if(board[yCoord,xCoor]==ENEMY)//ship already built
 		{
 			return 2;
 		}
-		else
+		else//can't build
 		{
 			return 0;
 		}
