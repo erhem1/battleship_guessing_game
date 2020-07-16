68d19727fe6a165d341b03f89d20c720a7ea918f
syintigs <erhembaatardashdorj@gmail.com>
2020-07-16 14:49:31 +0800

    EnemyAI imporvement

--- EnemyAI.c
+++ EnemyAI.c
@@ -3,17 +3,18 @@
 #include <time.h>
 #include "GlobalVariables.h"
 
-int ai_x(int middle_position, int difficulty)
+int ai_x( int difficulty)
 {
 	int x = 0; 
 	srand(time(NULL));
+	int ship_number = rand() % 5;
 	if( (rand() % 2) == 0 )  // 50% chance to add the random number to middle_position
 	{
 		while(1)
 		{
-			if(difficulty == EASY)          x = middle_position + (rand() % 4);
-			else if(difficulty == MEDIUM)   x = middle_position + (rand() % 3);
-			else if(difficulty == HARD)     x = middle_position + (rand() % 2);
+			if(difficulty == EASY)          x = middle_position[ship_number] + (rand() % 4);
+			else if(difficulty == MEDIUM)   x = middle_position[ship_number] + (rand() % 3);
+			else if(difficulty == HARD)     x = middle_position[ship_number] + (rand() % 2);
 			else return -1; // Not a proper difficulty as argument
 			if( ( x>= 0 ) && ( x<100) ) return x;
 		}
@@ -22,9 +23,9 @@ int ai_x(int middle_position, int difficulty)
 	{
 		while(1)
 		{
-			if(difficulty == EASY)          x = middle_position - (rand() % 4);
-			else if(difficulty == MEDIUM)   x = middle_position - (rand() % 3);
-			else if(difficulty == HARD)     x = middle_position - (rand() % 2);
+			if(difficulty == EASY)          x = middle_position[ship_number] - (rand() % 4);
+			else if(difficulty == MEDIUM)   x = middle_position[ship_number] - (rand() % 3);
+			else if(difficulty == HARD)     x = middle_position[ship_number] - (rand() % 2);
 			else return -1; // Not a proper difficulty as argument
 			if( ( x>= 0 ) && ( x<100) ) return x;
 		}
@@ -32,17 +33,18 @@ int ai_x(int middle_position, int difficulty)
 
 }
 
-int ai_y(int middle_position, int difficulty)
+int ai_y(int difficulty)
 {
 	int y = 0;
 	srand(time(NULL));
+	int ship_number = rand() % 5;
 	if( (rand() % 2) == 0 ) // 50% chance to add the random number to middle_position
 	{
 		while(1)
 		{
-			if(difficulty == EASY)         y = middle_position + (rand() % 4);
-			else if(difficulty == MEDIUM)  y = middle_position + (rand() % 3);
-			else if(difficulty == HARD)    y = middle_position + (rand() % 2);
+			if(difficulty == EASY)         y = middle_position[ship_number] + (rand() % 4);
+			else if(difficulty == MEDIUM)  y = middle_position[ship_number] + (rand() % 3);
+			else if(difficulty == HARD)    y = middle_position[ship_number] + (rand() % 2);
 			else return -1; // Not a proper difficulty as argument
 			if( ( y>=0 ) && ( y<100 ) ) return y;
 		}
@@ -51,9 +53,9 @@ int ai_y(int middle_position, int difficulty)
 	{
 		while(1)
 		{
-			if(difficulty == EASY)         y = middle_position - (rand() %4);
-			else if(difficulty == MEDIUM)  y = middle_position - (rand() %3);
-			else if(difficulty == HARD)    y = middle_position - (rand() %2);
+			if(difficulty == EASY)         y = middle_position[ship_number] - (rand() %4);
+			else if(difficulty == MEDIUM)  y = middle_position[ship_number] - (rand() %3);
+			else if(difficulty == HARD)    y = middle_position[ship_number] - (rand() %2);
 			else return -1; // Not a proper difficulty as argument
 			if( ( y>=0 ) && ( y<100 ) ) return y;
 		}
